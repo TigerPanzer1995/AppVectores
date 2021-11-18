64261f35f89c3907ee10c4986cf647f2a5d34809
TigerPanzer1995 <cellsoherrera@gmail.com>
2021-11-17 22:09:21 -0600

    Versión de desarrollo 1.0.0.4 ISC. Celso Herrera Vázquez

--- app/src/main/java/ismael/itssat/appvectores/Formulario.java
+++ app/src/main/java/ismael/itssat/appvectores/Formulario.java
@@ -3,11 +3,15 @@ package ismael.itssat.appvectores;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Formulario extends AppCompatActivity {
     DrawerLayout drawerLayout;
+    Button btnFormularioCampo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class Formulario extends AppCompatActivity {
 
         //asignamos la variable
         drawerLayout= findViewById(R.id.drawer_layout);
+        btnFormularioCampo = findViewById(R.id.xmlbtnformulariocampo);
     }//fin del metodo oncreate
 
     public void ClickMenu(View view){
@@ -41,6 +46,18 @@ public class Formulario extends AppCompatActivity {
     public void ClickLogOut(View view){
         //cerramos la app
         Home.logout(this);
+    }
+
+    private void ClickFormularioCampo(Activity activity, Class aclass){
+        Intent intent = new Intent(activity, aclass);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
+    public void accionFormulari(View view){
+        ClickFormularioCampo(this, FormularioCampo.class);
     }
 
     @Override
